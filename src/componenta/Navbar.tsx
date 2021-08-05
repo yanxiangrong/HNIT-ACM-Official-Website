460742251724bffa8c3ca809c5a6f2184e297ca3
小荣 <yanxiangrong_2@outlook.com>
2021-08-05 17:30:55 +0800

    完善导航栏

--- src/componenta/Navbar.tsx
+++ src/componenta/Navbar.tsx
@@ -1,11 +1,17 @@
 import "./Navbar.css"
 import React from "react";
+import Logo from "../assets/hnit-acm-logo-left-right.png"
+
+interface menuItem {
+  name: string
+  href: string
+}
 
 export class Navbar extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
     this.scrollHandler = this.scrollHandler.bind(this)
-    this.state = {navBarCor: "#fff"}
+    this.state = {navBarCor: "#FFFFFF00"}
   }
 
   componentDidMount() {
@@ -20,11 +26,11 @@ export class Navbar extends React.Component<any, any> {
     this.last_known_scroll_position = window.scrollY;
     if (!this.ticking) {
       window.requestAnimationFrame(() => {
-        console.log(this.last_known_scroll_position)
-        if (this.last_known_scroll_position === 0) {
-          this.setState({navBarCor: "#fff"})
+        // console.log(this.last_known_scroll_position)
+        if (this.last_known_scroll_position < 200) {
+          this.setState({navBarCor: "#FFFFFF00"})
         } else {
-          this.setState({navBarCor: "#ef2528"})
+          this.setState({navBarCor: "#2E2E2EFF"})
         }
         this.ticking = false;
       });
@@ -35,10 +41,31 @@ export class Navbar extends React.Component<any, any> {
 
   render() {
     const {navBarCor} = this.state
+    const menuItems: menuItem[] = [
+      {name: "主页", href: "/"},
+      {name: "加入我们", href: "/"},
+      {name: "基地", href: "/"},
+      {name: "照片墙", href: "/"},
+      {name: "关于", href: "/"}]
+    const menu = menuItems.map((item) => {
+      return (
+        <li className={"navBar-menu-item"}>
+          <a className={"navBar-menu-link"} href={item.href}>{item.name}</a>
+        </li>
+      )
+    })
     return (
-      <div className={"navBar"} style={{color: navBarCor}}>
-        导航栏
-      </div>
+      <nav className={"navBar"} style={{backgroundColor: navBarCor}}>
+        <header className={"navBar-header"}>
+          <div className={"navBar-left"}>
+            <a className={"navBar-logo-link"} href={"/"}>
+              <img className={"navBar-logo"} src={Logo} alt={"HNIT-ACM"}/>
+              <title className={"navBar-logo-title"}>HNIT-ACM</title>
+            </a>
+          </div>
+          <ul className={"navBar-right navBar-menu"}>{menu}</ul>
+        </header>
+      </nav>
     )
   }
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,23 @@
+import "./Home.css"
+
 function Home() {
   return (
-    <div style={{background: "gray", zIndex: 1, height: 2000, position: "absolute", width: "100%"}}>
+    <div style={{zIndex: 0, height: 2000, position: "absolute", width: "100%"}}>
+      <div className={"videoHeader"}>
+        <div className={"headerContent"}>
+          <h1 className={"header"}>
+            欢迎加入我们的团队，一起学习
+          </h1>
+        </div>
+        <div className={"videoBackground"}>
+          <video className={"videoLoop"} autoPlay muted playsInline loop>
+            <source src={"/assets/How-will-Artificial-Intelligence-and-Internet-of-Things-change-the-world.mp4"}
+                    type={"video/mp4"}/>
+          </video>
+        </div>
+      </div>
       <h1>
-        主页
+        aaa
       </h1>
     </div>
   )
